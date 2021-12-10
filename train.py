5e906c8b883720a3f330c5f44cda6481c1cef36f
constConstantinople <55313971+constConstantinople@users.noreply.github.com>
2021-12-10 14:22:12 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -8,7 +8,19 @@ from model import CycleGAN
 import time
 from arguments import Arguments
 from dataset import FaceDataSet
+from PIL import Image
 
+def tensor2img(img):
+    if not isinstance(img, np.ndarray):
+      if isinstance(img, torch.Tensor):
+        img = img.data
+      img = img[0].cpu().float().numpy()
+      img = (np.transpose(img, (1, 2, 0)) + 1)/2.0 * 255
+    return img.astype(np.uint8)
+
+def img_save(img, path, name):
+    img = Image.fromarray(img)
+    img.save(os.path.join(path, name))
 
 def train(dataset, model, config):
     train_loader, val_loader = dataset.train_loader, dataset.val_loader
@@ -18,32 +30,58 @@ def train(dataset, model, config):
         # epoch_start = time.time()
         
         for batch_data in train_loader:
-            print(len(batch_data))
+            # print(len(batch_data))
             A = batch_data[0]
             B = batch_data[1]
-            print(type(A),A.size())
-            print(type(B),B.size())
             # iter_start = time.time()
             n_iter += config.batch_size
+            
             model.Optimize(A, B)
             
             if n_iter % config.print_freq == 0:
                 print('print loss after %d iter' % (n_iter))
                 print("G_A: %f, G_B: %f, D_A: %f, D_B: %f, cycle_A: %f, cycle_B: %f" % (model.genLoss_A,model.genLoss_B,model.disLoss_A,model.disLoss_B,model.cycleLoss_A,model.cycleLoss_B))
-
+            
         if epoch % config.save_freq == 0:
             print('saving the model, epoch %d' % (epoch))
-            model.model_save(epoch)
+            # model.model_save(epoch)
             # TO DO: print pictures from model.real_A,fake_B,rec_A,real_B,fake_A,rec_B
+            real_A, real_B = tensor2img(model.real_A), tensor2img(model.real_B)
+            fake_A, fake_B = tensor2img(model.fake_A), tensor2img(model.fake_B)
+            rec_A, rec_B = tensor2img(model.fake_A), tensor2img(model.fake_B)
+            
+            print("Saving pictures at epoch", epoch)
+            
+            fig = plt.figure()
+            ax = fig.add_subplot(3, 2, 1)
+            ax.imshow(real_A)
+            ax = fig.add_subplot(3, 2, 2)
+            ax.imshow(real_B)
+            ax = fig.add_subplot(3, 2, 3)
+            ax.imshow(fake_A)
+            ax = fig.add_subplot(3, 2, 4)
+            ax.imshow(fake_B)
+            ax = fig.add_subplot(3, 2, 5)
+            ax.imshow(rec_A)
+            ax = fig.add_subplot(3, 2, 6)
+            ax.imshow(rec_B)
+            plt.show()
+            
+            img_save(real_A, config.print_dir, str(epoch) + '_real_A')
+            img_save(real_B, config.print_dir, str(epoch) + '_real_B')
+            img_save(fake_A, config.print_dir, str(epoch) + '_fake_A')
+            img_save(fake_B, config.print_dir, str(epoch) + '_fake_B')
+            img_save(rec_A, config.print_dir, str(epoch) + '_rec_A')
+            img_save(rec_B, config.print_dir, str(epoch) + '_rec_B')
 
         model.lr_update()
 
-        G_A += model.genLoss_A
-        G_B += model.genLoss_A
-        D_A += model.disLoss_A
-        D_B += model.disLoss_B
-        cycle_A += model.cycleLoss_A
-        cycle_B += model.cycleLoss_B
+        G_A.append(model.genLoss_A)
+        G_B.append(model.genLoss_A)
+        D_A.append(model.disLoss_A)
+        D_B.append(model.disLoss_B)
+        cycle_A.append(model.cycleLoss_A)
+        cycle_B.append(model.cycleLoss_B)
 
     plt.title('G_A')
     plt.plot(G_A)
